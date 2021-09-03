bb087e3f9938f8b835de95df117e337f4f01685d
true_mogician <925269713@qq.com>
2021-09-03 18:22:12 +0800

    fix: exception handling

--- src/index.ts
+++ src/index.ts
@@ -70,65 +70,67 @@ process.chdir((process as any).pkg
 	: __dirname);
 
 Http.createServer(async (req, res) => {
-	if (req.url != "/dnspod") {
-		send(res, 404);
-		return;
-	}
-	if (req.method != "POST") {
-		send(res, 405);
-		return;
-	}
 	try {
-		const body = await parseBody<Body>(req);
-		const recordsResp = JSON.parse(await Request.post("https://dnsapi.cn/Record.List", {
-			form: {
-				login_token: `${body.tokenId},${body.tokenValue}`,
-				format: "json",
-				domain: body.domain,
-				sub_domain: body.subDomain ?? "@",
-				record_type: body.ipVersion == 6 ? "AAAA" : "A"
+		if (req.url == "/dnspod") {
+			if (req.method != "POST") {
+				send(res, 405);
+				return;
 			}
-		})) as {
-			status: DnspodStatus,
-			records?: Record[]
-		};
-		if (recordsResp.status.code != "1") {
-			res.setHeader("Content-Type", "text/plain");
-			send(res, 400, recordsResp.status.message);
-			return;
-		}
-		recordsResp.records!.sort((a, b) => (b.weight ?? 0) - (a.weight ?? 0));
-		const record = recordsResp.records![0];
-		if (record.value == body.ip) {
-			res.setHeader("Content-Type", "text/plain");
-			send(res, 200, `${body.subDomain ? body.subDomain + '.' : ''}${body.domain}的解析值没有变化，无需更新`);
-			return;
-		}
-		const modifyResp = JSON.parse(await Request.post("https://dnsapi.cn/Record.Modify", {
-			form: {
-				login_token: `${body.tokenId},${body.tokenValue}`,
-				format: "json",
-				domain: body.domain,
-				record_id: record.id,
-				sub_domain: record.name,
-				record_type: record.type,
-				record_line_id: record.line_id,
-				value: body.ip,
-				mx: record.mx,
-				ttl: record.ttl,
-				status: record.status,
-				weight: record.weight
+			const body = await parseBody<Body>(req);
+			console.log(new Date().toTimeString(), body);
+			const recordsResp = JSON.parse(await Request.post("https://dnsapi.cn/Record.List", {
+				form: {
+					login_token: `${body.tokenId},${body.tokenValue}`,
+					format: "json",
+					domain: body.domain,
+					sub_domain: body.subDomain ?? "@",
+					record_type: body.ipVersion == 6 ? "AAAA" : "A"
+				}
+			})) as {
+				status: DnspodStatus,
+				records?: Record[]
+			};
+			if (recordsResp.status.code != "1") {
+				res.setHeader("Content-Type", "text/plain");
+				send(res, 400, recordsResp.status.message);
+				return;
 			}
-		})) as { status: DnspodStatus };
-		if (modifyResp.status.code != "1") {
-			res.setHeader("Content-Type", "text/plain");
-			send(res, 400, modifyResp.status.message);
-			return;
-		};
-		send(res, 200, `${body.subDomain ? body.subDomain + '.' : ''}${body.domain}的解析值成功从${record.value}更新为${body.ip}`);
+			recordsResp.records!.sort((a, b) => (b.weight ?? 0) - (a.weight ?? 0));
+			const record = recordsResp.records![0];
+			if (record.value == body.ip) {
+				res.setHeader("Content-Type", "text/plain");
+				send(res, 200, `${body.subDomain ? body.subDomain + '.' : ''}${body.domain}的解析值没有变化，无需更新`);
+				return;
+			}
+			const modifyResp = JSON.parse(await Request.post("https://dnsapi.cn/Record.Modify", {
+				form: {
+					login_token: `${body.tokenId},${body.tokenValue}`,
+					format: "json",
+					domain: body.domain,
+					record_id: record.id,
+					sub_domain: record.name,
+					record_type: record.type,
+					record_line_id: record.line_id,
+					value: body.ip,
+					mx: record.mx,
+					ttl: record.ttl,
+					status: record.status,
+					weight: record.weight
+				}
+			})) as { status: DnspodStatus };
+			if (modifyResp.status.code != "1") {
+				res.setHeader("Content-Type", "text/plain");
+				send(res, 400, modifyResp.status.message);
+				return;
+			};
+			send(res, 200, `${body.subDomain ? body.subDomain + '.' : ''}${body.domain}的解析值成功从${record.value}更新为${body.ip}`);
+		}
+		else
+			send(res, 404);
 	}
 	catch (error) {
-		send(res, 500, error);
+		send(res, 500, typeof error == "string" ? error : error.toString());
+		console.log(new Date(), error);
 	}
 }).listen(Settings.port);
 
