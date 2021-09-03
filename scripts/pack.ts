1f53646770addfbdcaba872bdd1a0875dd24b4ca
true_mogician <925269713@qq.com>
2021-09-03 15:11:33 +0800

    fix: type error

--- scripts/pack.ts
+++ scripts/pack.ts
@@ -6,4 +6,4 @@ function getNodeMajorVersion(): number {
 	const version = process.version.substr(1);
 	return Number.parseInt(version.substring(0, version.indexOf(".")));
 }
-exec(`pkg build/index.js -t node${getNodeMajorVersion()}-win-${process.arch} -o build/${Settings.exeName}.exe`, execCallback);
+exec(`pkg build/index.js -t node${getNodeMajorVersion()}-win-${process.arch} -o build/${Settings.exeName}.exe`, execCallback as any);
--- scripts/register.ts
+++ scripts/register.ts
@@ -4,4 +4,4 @@ import Settings from "../appSettings.json";
 
 exec(`New-Service -Name ${Settings.serviceName} -BinaryPathName build/${Settings.exeName}.exe`, {
 	shell: "powershell.exe"
-}, execCallback);
+}, execCallback as any);
