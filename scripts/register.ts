b3394173652e26240b33b585ae004537b63ad27a
true_mogician <925269713@qq.com>
2021-09-03 16:31:55 +0800

    fix: absolute binraypath

--- scripts/register.ts
+++ scripts/register.ts
@@ -2,6 +2,7 @@ import { exec } from "child_process";
 import { execCallback } from "./common";
 import Settings from "../appSettings.json";
 
-exec(`New-Service -Name ${Settings.serviceName} -BinaryPathName build/${Settings.exeName}.exe`, {
+const service = Settings.service;
+exec(`New-Service -Name ${service.name} -BinaryPathName ${process.cwd()}/build/${Settings.exeName}.exe -DisplayName ${service.displayName} -Description ${service.description} -StartupType ${service.startupType}`, {
 	shell: "powershell.exe"
 }, execCallback as any);
