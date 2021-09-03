3f86708f62bfa834dd070ee7133dfd9ac54c4612
true_mogician <925269713@qq.com>
2021-09-03 16:52:29 +0800

    fix: command escape

--- scripts/register.ts
+++ scripts/register.ts
@@ -3,6 +3,11 @@ import { execCallback } from "./common";
 import Settings from "../appSettings.json";
 
 const service = Settings.service;
-exec(`New-Service -Name ${service.name} -BinaryPathName ${process.cwd()}/build/${Settings.exeName}.exe -DisplayName ${service.displayName} -Description ${service.description} -StartupType ${service.startupType}`, {
+function escapeShell(command: string) {
+	if (!/[ "'$`\\]/.test(command))
+		return command;
+	return `"${command.replace(/(["'$`\\])/g, '\\$1')}"`;
+}
+exec(`New-Service -Name ${service.name} -BinaryPathName ${escapeShell(process.cwd() + "/build/" + Settings.exeName + ".exe")} -DisplayName ${escapeShell(service.displayName)} -Description ${escapeShell(service.description)} -StartupType ${service.startupType}`, {
 	shell: "powershell.exe"
 }, execCallback as any);
