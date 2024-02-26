d77128512eb0e09fe8677654ffaee40b1c7317f2
true_mogician <truemogician@gmail.com>
2024-02-26 13:33:27 -0800

    refactor: use axios instead of request-promise
    Refine type definitions;
    Fix some small bugs.

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,62 @@
 import "@basic-type-extensions/array";
 import Http from "http";
 import Path from "path";
-import Request from "request-promise";
+import axios, { AxiosError } from "axios";
 import config from "../config/server.config.json";
 
-type NumString = string;
-type DateString = string;
+namespace Dnspod {
+	type NumString = string;
+	type DateString = string;
 
-interface Body {
-	domain: string;
+	export interface Status {
+		code: NumString,
+		message: string,
+		created_at: DateString
+	}
 
-	token: {
-		id: number,
-		value: string
-	};
+	export interface Record {
+		id: NumString,
+		ttl: NumString,
+		value: string,
+		enabled: "0" | "1",
+		status: "enable" | "disable",
+		updated_on: DateString,
+		name: string,
+		line: string,
+		line_id: NumString,
+		type: "A" | "CNAME" | "MX" | "TXT" | "NS" | "AAAA" | "SPF" | "SRV" | "CAA" | "显性URL" | "隐性URL",
+		weight: number | null,
+		monitor_status: string,
+		remark: "string",
+		mx: NumString
+	}
 
-	oldIp: string;
-
-	newIp: string;
-}
-interface Record {
-	id: NumString,
-	ttl: NumString,
-	value: string,
-	enabled: "0" | "1",
-	status: "enable" | "disable",
-	updated_on: DateString,
-	name: string,
-	line: string,
-	line_id: NumString,
-	type: "A" | "CNAME" | "MX" | "TXT" | "NS" | "AAAA" | "SPF" | "SRV" | "CAA" | "显性URL" | "隐性URL",
-	weight: number | null,
-	monitor_status: string,
-	remark: "string",
-	mx: NumString
-}
-interface DnspodStatus {
-	code: NumString,
-	message: string,
-	created_at: DateString
+	export type Response<T extends object = {}> = T & { status: Status };
 }
 
-function isBody(body: any): body is Body {
-	return body && typeof body === "object" &&
-		typeof body.domain === "string" &&
-		typeof body.token === "object" &&
-		typeof body.token.id === "number" &&
-		typeof body.token.value === "string" &&
-		typeof body.oldIp === "string" &&
-		typeof body.newIp === "string";
+namespace Ddns {
+	export interface UpdateRecordsBody {
+		domain: string;
+
+		token: {
+			id: number,
+			value: string
+		};
+
+		oldIp: string;
+
+		newIp: string;
+	}
+
+	export function isUpdateRecordsBody(body: any): body is UpdateRecordsBody {
+		return body && typeof body === "object" &&
+			typeof body.domain === "string" &&
+			typeof body.token === "object" &&
+			typeof body.token.id === "number" &&
+			typeof body.token.value === "string" &&
+			typeof body.oldIp === "string" &&
+			typeof body.newIp === "string";
+	}
 }
 
 function receiveBody(req: Http.IncomingMessage): Promise<string> {
@@ -73,11 +81,6 @@ function receiveBody(req: Http.IncomingMessage): Promise<string> {
 	});
 }
 
-function send(req: Http.ServerResponse, statusCode: number, data?: any) {
-	req.statusCode = statusCode;
-	req.end(data);
-}
-
 process.chdir((process as any).pkg
 	? Path.resolve(process.execPath, "..")
 	: __dirname
@@ -85,63 +88,83 @@ process.chdir((process as any).pkg
 
 const ipv4Pattern = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/;
 Http.createServer(async (req, res) => {
+	function send(statusCode: number, data?: any) {
+		res.statusCode = statusCode;
+		res.end(data);
+	}
+
+	function handleError(error: any) {
+		const msg = error instanceof AxiosError
+			? error.response?.data ?? error.message
+			: error instanceof Error
+				? error.message
+				: error;
+		console.error(msg);
+		send(500, msg);
+	}
+
 	try {
 		switch (req.url) {
-			case "my-ip": {
+			case "/my-ip": {
 				if (req.method !== "GET") {
-					send(res, 405);
+					send(405);
 					return;
 				}
 				const ip = req.headers["x-real-ip"] || req.socket.remoteAddress;
 				res.setHeader("Content-Type", "text/plain");
-				send(res, 200, ip);
+				send(200, ip);
 				break;
 			}
 			case "/dnspod/update": {
 				if (req.method != "POST") {
-					send(res, 405);
+					send(405);
 					return;
 				}
 				res.setHeader("Content-Type", "text/plain");
 				const string = await receiveBody(req);
 				const body = JSON.parse(string);
-				if (!isBody(body)) {
-					send(res, 400, "Invalid body");
+				if (!Ddns.isUpdateRecordsBody(body)) {
+					send(400, "Invalid body");
 					return;
 				}
 				const { domain, token, oldIp, newIp } = body;
 				if (!ipv4Pattern.test(oldIp) || !ipv4Pattern.test(newIp)) {
-					send(res, 400, "Invalid IP");
+					send(400, "Invalid IP");
 					return;
 				}
 				if (oldIp == newIp) {
-					send(res, 400, "IP not changed");
+					send(400, "IP not changed");
 					return;
 				}
 				console.log(new Date().toTimeString(), body);
-				const recordsResp = JSON.parse(await Request.post("https://dnsapi.cn/Record.List", {
-					form: {
+				const recordsResp = await axios.post<Dnspod.Response<{ records?: Dnspod.Record[] }>>(
+					"https://dnsapi.cn/Record.List",
+					{
 						login_token: `${token.id},${token.value}`,
 						format: "json",
 						domain
+					},
+					{
+						headers: {
+							"Content-Type": "application/x-www-form-urlencoded"
+						}
 					}
-				})) as {
-					status: DnspodStatus,
-					records?: Record[]
-				};
-				if (recordsResp.status.code != "1") {
-					send(res, 500, recordsResp.status.message);
+				);
+				if (recordsResp.status != 200 || recordsResp.data.status.code != "1") {
+					send(500, recordsResp.data.status.message);
 					return;
 				}
-				recordsResp.records!.sort((a, b) => (b.weight ?? 0) - (a.weight ?? 0));
-				const records = recordsResp.records!.filter(r => r.value == body.oldIp);
-				if (records.length == 0) {
-					send(res, 200, "No record to update");
+				let records = recordsResp.data.records;
+				records?.sort((a, b) => (b.weight ?? 0) - (a.weight ?? 0));
+				records = records?.filter(r => r.value == body.oldIp);
+				if (!records || records.length == 0) {
+					send(200, "No record to update");
 					return;
 				}
-				const results = await records.mapAsync(
-					record => Request.post("https://dnsapi.cn/Record.Modify", {
-						form: {
+				for (const record of records) {
+					const resp = await axios.post<Dnspod.Response>(
+						"https://dnsapi.cn/Record.Modify",
+						{
 							login_token: `${token.id},${token.value}`,
 							format: "json",
 							domain,
@@ -154,31 +177,27 @@ Http.createServer(async (req, res) => {
 							ttl: record.ttl,
 							status: record.status,
 							weight: record.weight
+						},
+						{
+							headers: {
+								"Content-Type": "application/x-www-form-urlencoded"
+							}
 						}
-					}).then(r => JSON.parse(r) as { status: DnspodStatus }),
-					(error: any) => {
-						console.error(error);
-						send(res, 500, error);
-						return undefined;
+					);
+					if (resp.status != 200 || resp.data.status.code != "1") {
+						send(500, resp.data.status.message);
+						return;
 					}
-				);
-				if (results === undefined)
-					return;
-				const errorResult = results.find(r => r.status.code != "1");
-				if (errorResult) {
-					send(res, 500, errorResult.status.message);
-					return;
 				}
-				send(res, 200, `Updated ${results.length} records`);
+				send(200, `Updated ${records.length} records`);
 				break;
 			}
 			default:
-				send(res, 404);
+				send(404);
 		}
 	}
 	catch (error) {
-		console.error(error);
-		send(res, 500, error);
+		handleError(error);
 	}
 }).listen(config.port);
 
